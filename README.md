043fe291a0d168d2ede73bf1a0287bd9d609f5fa
Bhavyai Gupta <zbhavyai@gmail.com>
2022-04-19 22:16:15 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Repository created for demonstration in the discussion at [https://github.com/quarkusio/quarkus/discussions/25024](https://github.com/quarkusio/quarkus/discussions/25024).
+
+Quarkus guide: https://quarkus.io/guides/microprofile-graphql
---
+++ src/main/java/org/acme/microprofile/graphql/FilmResource.java
@@ -0,0 +1,64 @@
+package org.acme.microprofile.graphql;
+
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.DefaultValue;
+import org.eclipse.microprofile.graphql.Source;
+
+import javax.inject.Inject;
+import java.util.List;
+import io.smallrye.mutiny.operators.multi.processors.BroadcastProcessor;
+import io.smallrye.graphql.api.Subscription;
+import io.smallrye.mutiny.Multi;
+
+@GraphQLApi
+public class FilmResource {
+
+    @Inject
+    GalaxyService service;
+
+    BroadcastProcessor<Hero> processor = BroadcastProcessor.create();
+
+    @Query("allFilms")
+    @Description("Get all Films from a galaxy far far away")
+    public List<Film> getAllFilms() {
+        return service.getAllFilms();
+    }
+
+    @Query
+    @Description("Get a Films from a galaxy far far away")
+    public Film getFilm(@Name("filmId") int id) {
+        return service.getFilm(id);
+    }
+
+    public List<Hero> heroes(@Source Film film) {
+        return service.getHeroesByFilm(film);
+    }
+
+    @Mutation
+    public Hero createHero(Hero hero) {
+        service.addHero(hero);
+        processor.onNext(hero);
+        return hero;
+    }
+
+    @Subscription
+    public Multi<Hero> heroCreated() {
+        System.out.println("\nSubscription method called");
+        return processor;
+    }
+
+    @Mutation
+    public Hero deleteHero(int id) {
+        return service.deleteHero(id);
+    }
+
+    @Query
+    public List<Hero> getHeroesWithSurname(@DefaultValue("Skywalker") String surname) {
+        return service.getHeroesBySurname(surname);
+    }
+
+}
